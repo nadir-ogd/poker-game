fa4c190fe6aef58f5a2f4d66984ccb86fc1495ac
nadir-ogd <jn_ouagued@esi.dz>
2023-04-09 16:17:55 +0200

    strategies done

--- main.cpp
+++ main.cpp
@@ -5,19 +5,31 @@ int main() {
     // dealer d;
     // d.play();
     hand h1,h2;
-    for(int i = 8; i < 12; i++){
-        card c(Trefles,static_cast<Rang>(i));
-        h1.setCards(c);
-    }        
-    card c(Coeurs, King);
-    card c1(Piques, King);
-    card c2(Carreaux, King);  
-    h1.setCards(c);  
-    h2.setCards(c1);
-    h2.setCards(c2);
+    // for(int i = 3; i < 7; i++){
+    //     card c(Trefles,static_cast<Rang>(i));
+    //     h1.setCards(c);
+    // }        
+    card c1(Coeurs, Ten);
+    card c2(Trefles,Ace);
+    card c3(Piques, Ten);
+    card c4(Piques,Ace);
+    card c5(Piques,Jack);
+    card c6(Piques, King);
+    card c7(Piques, Deuce);  
+    h1.setCards(c1);  
+    h1.setCards(c2);  
+    h1.setCards(c3);  
+    h1.setCards(c4);  
+    h1.setCards(c5);  
+
+    h2.setCards(c6);
+    h2.setCards(c7);
     h2.print_hand();
     cout << """""""""" << endl;
-    cout << h1.is_4_of_a_kind(h2) << endl;
+    h1.print_hand();
+    cout << """""""""" << endl;
+    cout << h2.is_one_pair(h1) << endl;
+
     //cout << h1.is_royal_flush(h2) << endl;
     //cout << h1.is_straight_flush(h2) << endl;
 }
--- src/hand.cpp
+++ src/hand.cpp
@@ -29,15 +29,13 @@ int hand::sizeBroad()
     return hand_cards.size();
 }
 
-bool hand::is_royal_flush(hand hand_player)
+bool hand::is_royal_flush(hand hand_board)
 {
-    hand_cards.push_back(hand_player.get_card(0));
-    hand_cards.push_back(hand_player.get_card(1));
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
     sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
         return a.getCouleur() < b.getCouleur() ||
                (a.getCouleur() == b.getCouleur() && a.getRang() < b.getRang());
     });
-    print_hand();
     for (int i = 0; i < 3; i++) {
         if (hand_cards[i].getCouleur() == hand_cards[i + 1].getCouleur() &&
             hand_cards[i].getCouleur() == hand_cards[i + 2].getCouleur() &&
@@ -56,15 +54,13 @@ bool hand::is_royal_flush(hand hand_player)
     
 }
 
-bool hand::is_straight_flush(hand hand_player)
+bool hand::is_straight_flush(hand hand_board)
 {
-    hand_cards.push_back(hand_player.get_card(0));
-    hand_cards.push_back(hand_player.get_card(1));
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
     sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
         return a.getCouleur() < b.getCouleur() ||
                (a.getCouleur() == b.getCouleur() && a.getRang() < b.getRang());
     });
-    print_hand();
     for (int i = 0; i < 3; i++) {
         if (hand_cards[i].getCouleur() == hand_cards[i + 1].getCouleur() &&
             hand_cards[i].getCouleur() == hand_cards[i + 2].getCouleur() &&
@@ -88,70 +84,99 @@ bool hand::is_straight_flush(hand hand_player)
     
 }
 
-bool hand::is_4_of_a_kind(hand hand_player)
+bool hand::is_4_of_a_kind(hand hand_board)
 {
-    hand_cards.push_back(hand_player.get_card(0));
-    hand_cards.push_back(hand_player.get_card(1));
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
     sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
                return a.getRang() < b.getRang();
     });
 
-    print_hand();
     for (int i = 0; i < 4; i++) {
         if (hand_cards[i].getRang() == hand_cards[i + 1].getRang() &&
             hand_cards[i].getRang() == hand_cards[i + 2].getRang() &&
-            hand_cards[i].getRang() == hand_cards[i + 3].getRang() &&
-            hand_cards[i].getRang() == hand_cards[i + 3].getRang())
+            hand_cards[i].getRang() == hand_cards[i + 3].getRang()
+            )
                 return true;
         }   
     return false;
 }
 
 
-bool hand::is_full_house(hand hand_player)
+bool hand::is_full_house(hand hand_board)
 {
-    hand_cards.push_back(hand_player.get_card(0));
-    hand_cards.push_back(hand_player.get_card(1));
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
+    sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
+               return a.getRang() < b.getRang();
+    });
+    
+}
+
+bool hand::is_flush(hand hand_board)
+{
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
+    sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
+        return a.getCouleur() < b.getCouleur();
+    });
+    for (int i = 0; i < 3; i++) {
+        if (hand_cards[i].getCouleur() == hand_cards[i + 1].getCouleur() &&
+            hand_cards[i].getCouleur() == hand_cards[i + 2].getCouleur() &&
+            hand_cards[i].getCouleur() == hand_cards[i + 3].getCouleur() &&
+            hand_cards[i].getCouleur() == hand_cards[i + 4].getCouleur())
+                return true;
+    }
+    return false;
+    
+
+}
+
+bool hand::is_straight(hand hand_board)
+{
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
+    sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
+        return a.getCouleur() < b.getCouleur() ||
+               (a.getCouleur() == b.getCouleur() && a.getRang() < b.getRang());
+    });
+    for (int i = 0; i < 3; i++) {
+        if (
+            (hand_cards[i].getRang() == hand_cards[i + 1].getRang() - 1 &&
+            hand_cards[i].getRang() == hand_cards[i + 2].getRang() - 2 &&
+            hand_cards[i].getRang() == hand_cards[i + 3].getRang() - 3 &&
+            hand_cards[i].getRang() == hand_cards[i + 4].getRang() - 4 &&
+            hand_cards[i+4].getRang() != 12) || 
+            (hand_cards[i].getRang() == 0 &&
+            hand_cards[i+1].getRang() == 1 &&
+            hand_cards[i+2].getRang() == 2 &&
+            hand_cards[i+3].getRang() == 3 &&
+            hand_cards[i+4].getRang() == 12))
+            {
+            return true;
+        }   
+    }
+    return false;
+}
+
+
+bool hand::is_3_of_a_kind(hand hand_board)
+{
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
     sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
                return a.getRang() < b.getRang();
     });
 
-
-}
-
-bool hand::is_flush(hand hand_player)
-{
-    
-}
-
-bool hand::is_straight(hand hand_player)
-{
-    
-}
-
-
-bool hand::is_3_of_a_kind(hand hand_player)
-{
- hand_cards.push_back(hand_player.get_card(0));
-    hand_cards.push_back(hand_player.get_card(1));
-    sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
-               return a.getRang() < b.getRang();
-    });
-
-    print_hand();
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 5; i++) {
         if (hand_cards[i].getRang() == hand_cards[i + 1].getRang() &&
-            hand_cards[i].getRang() == hand_cards[i + 2].getRang() &&
-            hand_cards[i].getRang() == hand_cards[i + 3].getRang() &&
-            hand_cards[i].getRang() == hand_cards[i + 3].getRang())
+            hand_cards[i].getRang() == hand_cards[i + 2].getRang())
                 return true;
         }   
     return false;
 }
 
-bool hand::is_two_pairs(hand hand_player)
+bool hand::is_two_pairs(hand hand_board)
 {
-    //sort_hand(hand_player); 
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
+    sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
+               return a.getRang() < b.getRang();
+    });   
     int cpt = 0;
     for (int i = 0; i < 6; i++) {
         if (hand_cards[i].getRang() == hand_cards[i+1].getRang()) {
@@ -165,9 +190,12 @@ bool hand::is_two_pairs(hand hand_player)
         return false;  
 }
 
-bool hand::is_one_pair(hand hand_player)
+bool hand::is_one_pair(hand hand_board)
 {
-    //sort_hand(hand_player);
+    hand_cards.insert(hand_cards.end(), hand_board.hand_cards.begin(), hand_board.hand_cards.end());
+    sort(hand_cards.begin(), hand_cards.end(), [](card& a, card& b) {
+               return a.getRang() < b.getRang();
+    });
     for (int i = 0; i < 6; i++) {
         if (hand_cards[i].getRang() == hand_cards[i+1].getRang()) {
             return true;
@@ -176,7 +204,7 @@ bool hand::is_one_pair(hand hand_player)
     return false;
 }
 
-bool hand::is_high_card(hand hand_player)
+bool hand::is_high_card(hand hand_board)
 {
-    
+
 }
